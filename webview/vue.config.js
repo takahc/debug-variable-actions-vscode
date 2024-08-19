16905a89bf4a86b45ad2d1cec63034d3aeb084a3
akipg <akipg.dev@gmail.com>
2024-08-19 22:48:03 +0900

    vue: Define __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ feature to true

--- webview/vue.config.js
+++ webview/vue.config.js
@@ -1,7 +1,13 @@
 const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true,
+      }),
+    ],
     optimization: {
       splitChunks: false,
       runtimeChunk: false,
