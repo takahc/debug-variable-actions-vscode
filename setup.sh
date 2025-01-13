76f44816ff0446e3e191b89e85c621ab1a8406cd
akipg <108157287+akipg@users.noreply.github.com>
2025-01-13 18:43:02 +0900

    feat: Enhance setup script to use npm in GitHub Actions environment

--- setup.sh
+++ setup.sh
@@ -1,16 +1,29 @@
 rm -rf node_modules
-pnpm i
-pnpm install --include=optional sharp
-pnpm install --config.platform=linux --config.architecture=x64 sharp
-pnpm install --config.platform=win32 --config.architecture=x64 sharp
+
+# use npm for GitHub Actions
+if [ -n "$GITHUB_ACTIONS" ]; then
+    echo "Use npm"
+    NPM=npm
+    npm i
+    npm install --include=optional sharp
+    npm install --os=linux --cpu=x64 sharp
+    npm install --os=win32 --cpu=x64 sharp
+else
+    echo "Use pnpm"
+    NPM=pnpm
+    pnpm i
+    pnpm install --include=optional sharp
+    pnpm install --config.platform=linux --config.architecture=x64 sharp
+    pnpm install --config.platform=win32 --config.architecture=x64 sharp
+fi
 
 
 pushd file-server
 rm -rf node_modules
-pnpm i
+$NPM i
 popd
 
 pushd webview
 rm -rf node_modules
-pnpm i
+$NPM i
 popd
